b607d2e1f807ad3ead2c23c99f0fce0284c344d0
ashwini <ashwinishahasane14@gmail.com>
2023-01-02 04:32:43 +0530

    Created model for searching all available gallery tags

--- app/src/main/java/com/ashwini/imgurgallery/SearchModel.kt
+++ app/src/main/java/com/ashwini/imgurgallery/SearchModel.kt
@@ -1,28 +1,68 @@
 package com.ashwini.imgurgallery
 
  class SearchModel {
-     var id: String? = null
-     var name: String? = null
-     var display_name : String?= null
+      // on below line we are creating a two variable
+      // one for course name and other for course image.
+      var id: String?= null
+      var name: String?= null
+      var display_name: String?= null
+      var followers: Int?= null
+      var total_items: Int?= null
+      var background_hash: String?= null
 
-     fun SearchModel() {}
-
-     fun SearchModel(id: String, name: String, display_name : String) {
-        this.id = id
-        this.name = name
-        this.display_name = display_name
-    }
-
-    fun getid(): String? {
-        return id
-    }
-
-    fun getname(): String? {
-        return name
-    }
-
-     fun getdisplay_name(): String?{
-         return display_name
+     fun getid(): String {
+         return id.toString()
      }
 
-}
+     fun setid(id: String) {
+         this.id = name
+     }
+
+     fun getnames(): String {
+         return name.toString()
+     }
+
+     fun setnames(name: String) {
+         this.name = name
+     }
+
+     fun getdisplay_name(): String {
+         return display_name.toString()
+     }
+
+     fun setdisplay_name(display_name: String) {
+         this.display_name = display_name
+     }
+
+     fun getfollowers(): Int {
+         return followers!!
+     }
+
+     fun setfollowers(followers: Int) {
+         this.followers = followers
+     }
+
+     fun gettotal_items(): Int {
+         return total_items!!
+     }
+
+     fun settotal_items(total_items: Int) {
+         this.total_items = total_items
+     }
+
+     fun getbackground_hash(): String {
+         return background_hash.toString()
+     }
+
+     fun setbackground_hash(background_hash: String) {
+         this.background_hash = background_hash
+     }
+
+
+
+
+
+
+
+ }
+
