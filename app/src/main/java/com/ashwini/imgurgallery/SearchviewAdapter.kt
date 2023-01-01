7e9a426b65f9419bf73c65d829b7382b16cb7571
ashwini <ashwinishahasane14@gmail.com>
2023-01-02 04:45:27 +0530

    Recyclerview adapter to diplay the tags

---
+++ app/src/main/java/com/ashwini/imgurgallery/SearchviewAdapter.kt
@@ -0,0 +1,46 @@
+package com.ashwini.imgurgallery
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+
+class SearchviewAdapter(private var searchList: ArrayList<SearchModel>, ) : RecyclerView.Adapter<SearchviewAdapter.CourseViewHolder>() {
+        
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchviewAdapter.CourseViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.search_item, parent, false)
+        return CourseViewHolder(itemView)
+    }
+        
+        
+        fun filterList(filterlist: ArrayList<SearchModel>) {
+           
+            searchList = filterlist
+            
+            notifyDataSetChanged()
+            }
+        
+        override fun onBindViewHolder(holder: SearchviewAdapter.CourseViewHolder, position: Int) {
+            
+            holder.id.text = searchList.get(position).id
+            holder.name.text= (searchList.get(position).name)
+            holder.display_name.text=(searchList.get(position).display_name)
+
+        }
+        
+        override fun getItemCount(): Int {
+            
+            return searchList.size
+            }
+        
+        class CourseViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+           
+            var id: TextView = itemView.findViewById(R.id.sid)
+            var name: TextView = itemView.findViewById(R.id.sname)
+            var display_name: TextView = itemView.findViewById(R.id.sdisplay_name)
+
+
+        }
+}
